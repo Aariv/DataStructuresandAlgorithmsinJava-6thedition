839c077206e527ee45e780ac3f0f7d510b4594ea
Ariv <arivbits@gmail.com>
2019-09-08 17:44:38 +0530

    Working on Stack

---
+++ src/com/dsalgo/chapter6/stacks/Stack.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.dsalgo.chapter6.stacks;
+
+/**
+ * @author zentere
+ *
+ */
+public class Stack {
+	public static int SIZE = 10;
+	public static int top = 0;
+
+	public static int[] s = new int[SIZE];
+
+	public static boolean isEmpty() {
+		if (top == 0)
+			return true;
+		return false;
+	}
+
+	public static void push(int x) {
+		top += 1;
+		if (top > SIZE)
+			System.out.println("StackOverflow");
+		else
+			s[top] = x;
+	}
+
+	public static int pop() {
+		if (isEmpty()) {
+			System.out.println("Stack is empty");
+			return -1;
+		} else
+			top -= 1;
+		return s[top + 1];
+	}
+
+	public static void main(String[] args) {
+		System.out.println(pop());
+		push(10);
+		push(11);
+		push(12);
+		push(13);
+		
+		System.out.println(pop());
+		System.out.println(pop());
+		System.out.println(pop());
+		System.out.println(pop());
+	}
+}
---
+++ src/com/dsalgo/chapter6/stacks/StackLinkedListDemo.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.dsalgo.chapter6.stacks;
+
+/**
+ * @author zentere
+ *
+ */
+public class StackLinkedListDemo {
+
+}
+
+class Node {
+	public int data;
+	public Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class LStack {
+	private Node head;
+	private Node top;
+
+	public LStack() {
+		head = null;
+		top = null;
+	}
+
+	public void traversal() {
+		Node temp = this.head;
+		while (temp != null) {
+			System.out.print(temp.data);
+			temp = temp.next;
+		}
+		System.out.println("");
+	}
+
+	public boolean isEmpty() {
+		if (this.top == null)
+			return true;
+		return false;
+	}
+}
